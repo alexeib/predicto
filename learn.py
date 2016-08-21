7e6c4434ee71464d118632922df4af566f98956d
Alex <alexei.b@gmail.com>
2016-08-21 13:17:33 +0100

    added learning schema + tests

--- learn.py
+++ learn.py
@@ -1,4 +1,38 @@
+from jsonschema import validate
+
+learn_schema = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "title": "Learning model creation schema",
+    "type": "object",
+    "properties": {
+        "data": {
+            "title": "Observations",
+            "type": "array",
+            "items": {
+                "title": "Observation",
+                "type": "object",
+                "properties": {
+                    "inputs": {
+                        "type": "array",
+                        "items": {
+                            "type": "number"
+                        }
+                    },
+                    "output": {
+                        "type": "number"
+                    }
+                },
+                "required": ["inputs", "output"]
+            }
+        }
+    },
+    "required": ["data"]
+}
+
+
 class Learn:
     @staticmethod
     def create_predictor(params):
+        print(params)
+        validate(params, learn_schema)
         return None
---
+++ tests/main_tests.py
@@ -0,0 +1,42 @@
+import unittest
+import main
+from flask import json
+from jsonschema import ValidationError
+
+
+class PredictoTestCase(unittest.TestCase):
+    def setUp(self):
+        main.app.config['TESTING'] = True
+        self.app = main.app.test_client(False)
+
+    def test_fails_on_wrong_data(self):
+        with self.assertRaises(Exception) as context:
+            self.app.post(
+                '/learn',
+                data=
+                json.dumps(dict(
+                    data=[
+                        {
+                            "inpuxts": [1, 2, 3],
+                            "output": 5
+                        }
+                    ]
+                )),
+                content_type='application/json')
+        self.assertTrue(isinstance(context.exception, ValidationError))
+
+    def test_succeeds_on_correct_data(self):
+        resp = self.app.post('/learn', data=json.dumps(dict(
+            data=[
+                {
+                    "inputs": [1, 2, 3],
+                    "output": 5
+                }
+            ]
+        )),
+                             content_type='application/json')
+        assert resp.status_code == 200
+
+
+if __name__ == '__main__':
+    unittest.main()
