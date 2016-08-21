696c87cd197a73b31f19b8451af3eee394365fcc
Alexei Baevski <alexei.b@gmail.com>
2016-08-21 12:18:58 +0100

    added stub learn class

---
+++ learn.py
@@ -0,0 +1,4 @@
+class Learn:
+    @staticmethod
+    def create_predictor(params):
+        return None
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, request
+from learn import Learn
 
 app = Flask(__name__)
 
 
 @app.route('/learn', methods=['POST'])
 def learn():
-    return 'Hello, World!'
+    params = request.get_json(force=False, silent=False, cache=False)
+    Learn.create_predictor(params)
+    return "Learned model"
 
 if __name__ == "__main__":
     app.run()
